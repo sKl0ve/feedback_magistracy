aa67d9c8193d0b793d751a7e7579fa5f5dc13189
sKl0ve <ezirov77@gmail.com>
2025-02-09 01:08:49 +0300

    update : add registration stage

--- main.py
+++ main.py
@@ -24,14 +24,14 @@ bot = telebot.TeleBot(read_file('token.ini'))
 def main(message):
     connect = sqlite3.connect('feedback.sql')
     cur = connect.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS data (id integer primary key, username varchar(50), fio varchar(50), specialization varchar(50), semester varchar(50), survey text)')
+    cur.execute('CREATE TABLE IF NOT EXISTS data (id integer, username varchar(50), fio varchar(50), specialization varchar(50), semester varchar(50), survey text)')
+    cur.execute('INSERT INTO data (id, username) VALUES (%d, "%s")' % (message.chat.id, message.chat.username))
     connect.commit()
     
-    check_user = cur.execute('SELECT fio FROM data WHERE id= %d' % message.chat.id)
-    user = check_user.fetchall()
-    print(type(user))
+    check_user = cur.execute('SELECT fio FROM data WHERE id= %d' % message.chat.id).fetchone()
+    print(type(check_user[0]))
     
-    if len(user) == 0:
+    if check_user[0] is None:
         btn = types.InlineKeyboardButton('Ввести ФИО', callback_data='enter_fio')
         first_message = f'Привет! \n\nЗдесь ты можешь пройти опрос о качестве предоставляемого обучения на корпоративных магистратурах и оставить отзыв о преподаваемых дисциплинах. \n\nДля того чтобы начать нажми на кнопку ниже и введи свои данные'
     else:
@@ -40,17 +40,6 @@ def main(message):
     keyboard= types.InlineKeyboardMarkup()
     keyboard.row(btn)
     bot.send_message(message.chat.id, first_message, reply_markup=keyboard)
-    # if len(user) == 0:
-    #     btn_registration = types.InlineKeyboardButton('Ввести ФИО', callback_data='enter_fio', )
-    # btn_complete_the_survey = types.InlineKeyboardButton('Пройти опрос', callback_data='complete_the_survey')
-    # inline_keyboard.row(btn_complete_the_survey)
-    # bot.send_message(message.chat.id, f'Привет! \nЗдесь ты можешь пройти опрос о качестве по качеству предоставляемого обучения на корпоративных магистратурах и оставить отзыв о преподаваемых дисциплинах.', parse_mode='html', reply_markup=inline_keyboard)
-    # bot.register_next_step_handler(message, wish_url_add)
-
-    # connect = sqlite3.connect('feedback.sql')
-    # cur = connect.cursor()
-    # cur.execute('REPLACE INTO data (id, username, fio) VALUES (%d, "%s", "%s")' % (message.chat.id, message.chat.username))
-    # connect.commit()
     cur.close()
     connect.close()
 
@@ -63,8 +52,22 @@ def survey(callback):
         for values in SPECIALIZATIONS.values():
             keyboard.add(types.InlineKeyboardButton(values, callback_data = "callback"))
         bot.send_message(callback.message.chat.id, "Выберите ваше направление:", reply_markup=keyboard )
-    #elif callback.data == 'enter_fio':
+    elif callback.data == 'enter_fio':
+        bot.edit_message_text('Отправь мне своё ФИО', callback.message.chat.id, callback.message.message_id)
+        bot.register_next_step_handler(callback.message, register_fio)
         
+        
+def register_fio(message):
+    connect = sqlite3.connect('feedback.sql')
+    cur = connect.cursor()
+    cur.execute('UPDATE data SET fio = "%s" WHERE id = %d ' % (message.text, message.chat.id))
+    connect.commit()
+    cur.close()
+    connect.close()
+    btn = types.InlineKeyboardButton('Пройти опрос', callback_data='complete_the_survey')
+    keyboard= types.InlineKeyboardMarkup()
+    keyboard.row(btn)
+    bot.send_message(message.chat.id, 'Отлично! Теперь ты можешь пройти опрос о качестве обучения', reply_markup=keyboard)
         
         
         
