4f860d0ba65349700ab06fedeae6c82866ba5f57
sKl0ve <ezirov77@gmail.com>
2025-02-15 12:19:25 +0300

    update : choosing a specialization/group added

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ def main(message):
     connect.commit()
     
     check_user = cur.execute('SELECT fio FROM data WHERE id= %d' % message.chat.id).fetchone()
-    print(type(check_user[0]))
     
     if check_user[0] is None:
         btn = types.InlineKeyboardButton('Ввести ФИО', callback_data='enter_fio')
@@ -48,13 +47,26 @@ def main(message):
 def survey(callback):
     if callback.data == 'complete_the_survey':
         bot.delete_message(callback.message.chat.id, callback.message.message_id)
-        keyboard = types.InlineKeyboardMarkup(row_width=1)
-        for values in SPECIALIZATIONS.values():
-            keyboard.add(types.InlineKeyboardButton(values, callback_data = "callback"))
-        bot.send_message(callback.message.chat.id, "Выберите ваше направление:", reply_markup=keyboard )
+        keyboard = specializations_buttons()
+        bot.send_message(callback.message.chat.id, "Выберите ваше направление:", reply_markup=keyboard)
     elif callback.data == 'enter_fio':
         bot.edit_message_text('Отправь мне своё ФИО', callback.message.chat.id, callback.message.message_id)
         bot.register_next_step_handler(callback.message, register_fio)
+    elif callback.data == 'specialization_09.04.04_04':
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        keyboard = group_code_buttons()
+        bot.send_message(callback.message.chat.id, 'Ты выбрал направление "ИТ – инфраструктура предприятия".\nТеперь выбери свою группу:', reply_markup=keyboard)
+    elif callback.data == 'specialization_01.04.03_04' or callback.data == 'specialization_01.04.03_06' or callback.data == 'specialization_10.04.01_04' or callback.data == 'specialization_38.04.01_30':
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        keyboard = specializations_buttons()
+        bot.send_message(callback.message.chat.id, 'Опрос для этого направления находится в разработка.\nСейчас доступно направление ИТ-инфраструктура предприятия', reply_markup=keyboard)
+    elif callback.data == 'first_course':
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        bot.send_message(callback.message.chat.id, 'Ты выбрал группу 5140904/40401')
+    elif callback.data == 'second_course':
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        keyboard = group_code_buttons()
+        bot.send_message(callback.message.chat.id, 'Опрос для этой группы находится в разработка.\nСейчас доступен опрос для группы 5140904/40401', reply_markup=keyboard)
         
         
 def register_fio(message):
@@ -68,10 +80,21 @@ def register_fio(message):
     keyboard= types.InlineKeyboardMarkup()
     keyboard.row(btn)
     bot.send_message(message.chat.id, 'Отлично! Теперь ты можешь пройти опрос о качестве обучения', reply_markup=keyboard)
-        
-        
-        
-        
+
+
+def specializations_buttons():
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    for values in SPECIALIZATIONS.values():
+        callback_message = "specialization_" + values[:11]
+        keyboard.add(types.InlineKeyboardButton(values, callback_data = callback_message))
+    return keyboard
     
-    
+        
+def group_code_buttons():
+    keyboard= types.InlineKeyboardMarkup(row_width=2)
+    btn_group_1_course = types.InlineKeyboardButton('5140904/40401', callback_data = 'first_course')
+    btn_group_2_course = types.InlineKeyboardButton('5140904/30401', callback_data = 'second_course')
+    keyboard.row(btn_group_1_course, btn_group_2_course)
+    return keyboard
+
 bot.infinity_polling(timeout=10, long_polling_timeout = 5)
