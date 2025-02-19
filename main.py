8f873084002a4a47bc53e923f10782e7f656efbb
sKl0ve <ezirov77@gmail.com>
2025-02-19 23:14:41 +0300

    downloadding data to DB added

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ SPECIALIZATIONS = {
     "38.04.01_30" : "Экономика ИТ и бизнес-анализ"
 }
 GROUPS = {
-    "01.04.03_04" : [],
-    "01.04.03_06" : [],
-    "09.04.04_04" : ['5140904/40401', '5140904/30401'],
-    "10.04.01_04" : [],
-    "38.04.01_30" : [],
+    "01.04.03_04" : {},
+    "01.04.03_06" : {},
+    "09.04.04_04" : {"1_course" : "5140904/40401", "2_course" : "5140904/30401"},
+    "10.04.01_04" : {},
+    "38.04.01_30" : {},
 }
 DISCIPLINES = {1 : ['Ин.яз. в проф. коммуникации', 'Цифровые ресурсы в НИ', 'Управление проектами', 'Верификация алгоритмов и систем', 'Построение ИТ инфраструктуры', 'ИТ инфраструктура предприятия', 'Внедрение и сопровождение ПП', 'Ознакомительная практика'],
                2 : [], 
@@ -34,19 +34,25 @@ bot = telebot.TeleBot(read_file('token.ini'))
 def main(message):
     connect = sqlite3.connect('feedback.sql')
     cur = connect.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS data (id integer primary key, username varchar(50), fio varchar(50), specialization varchar(50), semester varchar(50), survey text)')
-    cur.execute('INSERT OR IGNORE INTO data (id, username) VALUES (%d, "%s")' % (message.chat.id, message.chat.username))
+    cur.execute('CREATE TABLE IF NOT EXISTS student (chatid integer primary key, username varchar(50), fio varchar(80), specialization varchar(50), ciphergroup varchar(50))')
+    cur.execute('CREATE TABLE IF NOT EXISTS education (id integer primary key, chatid integer, semester text, discipline text, question1 text, question2 text, question3 text, question4 text, question5 text, FOREIGN KEY(chatid) REFERENCES student(chatid))')
+    cur.execute('CREATE TABLE IF NOT EXISTS practice (id integer primary key, chatid integer, semester text, question1 text, question2 text, question3 text, question4 text, question5 text, FOREIGN KEY(chatid) REFERENCES student(chatid))')
+    cur.execute('INSERT OR IGNORE INTO student (chatid, username) VALUES (%d, "%s")' % (message.chat.id, message.chat.username))
     connect.commit()
-    check_user = cur.execute('SELECT fio FROM data WHERE id= %d' % message.chat.id).fetchone()
+    check_user = cur.execute('SELECT fio, specialization, ciphergroup FROM student WHERE chatid= %d' % message.chat.id).fetchone()
     cur.close()
     connect.close()
-    if check_user[0] is None:
-        btn = types.InlineKeyboardButton('Ввести ФИО', callback_data='enter_fio')
-        first_message = f'Привет! \n\nЗдесь ты можешь пройти опрос о качестве предоставляемого обучения на корпоративных магистратурах и оставить отзыв о преподаваемых дисциплинах. \n\nДля того чтобы начать нажми на кнопку ниже и введи свои данные'
+    
+    if check_user[0] is None and check_user[1] is None and check_user[2] is None:
+        btn = types.InlineKeyboardButton('Зарегистрироваться', callback_data='registration')
+        first_message = f'Привет! \n\nТы здесь впервые, для того чтобы начать нужно пройти регистрацию.'
+    # elif None in check_user:
+    #     btn = types.InlineKeyboardButton('Продолжить регистрацию', callback_data='registration1')
+    #     first_message = f'Ты не прошёл регистрацию до конца, для того чтобы продолжить нужно закончить регистрацию.'
     else:
         btn = types.InlineKeyboardButton('Пройти опрос', callback_data='complete_the_survey')
-        first_message = f'Привет! \n\nЗдесь ты можешь пройти опрос о качестве предоставляемого обучения на корпоративных магистратурах и оставить отзыв о преподаваемых дисциплинах.'
-    keyboard= types.InlineKeyboardMarkup()
+        first_message = f'Ты уже зарегестрирован и  можешь пройти опрос о качестве предоставляемого обучения на корпоративной магистратуре.'
+    keyboard = types.InlineKeyboardMarkup()
     keyboard.row(btn)
     bot.send_message(message.chat.id, first_message, reply_markup=keyboard)
 
@@ -55,12 +61,16 @@ def main(message):
 def survey(callback):
     if callback.data == 'complete_the_survey':
         bot.delete_message(callback.message.chat.id, callback.message.message_id)
-        keyboard = specializations_buttons()
-        bot.send_message(callback.message.chat.id, "Выберите ваше направление:", reply_markup=keyboard)
-    elif callback.data == 'enter_fio':
-        bot.edit_message_text('Отправь мне своё ФИО', callback.message.chat.id, callback.message.message_id)
-        bot.register_next_step_handler(callback.message, register_fio)
+        semester_choice(callback)
+    elif callback.data == 'registration':
+        registration(callback)
     elif callback.data == '09.04.04_04':
+        connect = sqlite3.connect('feedback.sql')
+        cur = connect.cursor()
+        cur.execute('UPDATE student SET specialization = "%s" WHERE chatid = %d ' % (callback.data, callback.message.chat.id))
+        connect.commit()
+        cur.close()
+        connect.close()
         bot.delete_message(callback.message.chat.id, callback.message.message_id)
         keyboard = group_code_buttons(callback.data)
         bot.send_message(callback.message.chat.id, 'Ты выбрал направление "ИТ – инфраструктура предприятия".\nТеперь выбери свою группу:', reply_markup=keyboard)
@@ -70,6 +80,13 @@ def survey(callback):
         keyboard = specializations_buttons()
         bot.send_message(callback.message.chat.id, 'Опрос для этого направления находится в разработке.\nСейчас доступно направление ИТ-инфраструктура предприятия', reply_markup=keyboard)
     elif callback.data == '1_course':
+        connect = sqlite3.connect('feedback.sql')
+        cur = connect.cursor()
+        specialization = cur.execute('SELECT specialization FROM student WHERE chatid = %d' % callback.message.chat.id).fetchone()
+        cur.execute('UPDATE student SET ciphergroup = "%s" WHERE chatid = %d ' % (GROUPS[specialization[0]][callback.data], callback.message.chat.id))
+        connect.commit()
+        cur.close()
+        connect.close()
         bot.delete_message(callback.message.chat.id, callback.message.message_id)
         semester_choice(callback)
     elif callback.data == '2_course':
@@ -78,8 +95,7 @@ def survey(callback):
         keyboard = group_code_buttons('09.04.04_04') 
         bot.send_message(callback.message.chat.id, 'Опрос для этой группы находится в разработке.\nСейчас доступен опрос для группы 5140904/40401', reply_markup=keyboard)
     elif callback.data == '1_semester':
-        bot.delete_message(callback.message.chat.id, callback.message.message_id)
-        print(callback)
+        bot.delete_message(callback.message.chat.id, callback.message.message_id) 
         discplines_choice(callback) 
     # elif callback.data == '2_semester' or callback.data == '3_semester' or callback.data == '4_semester':
     #     bot.delete_message(callback.message.chat.id, callback.message.message_id)
@@ -87,25 +103,34 @@ def survey(callback):
     #     bot.send_message(callback.message.chat.id, 'Опрос для этого семестра находится в разработке.\nСейчас доступен опрос для 1-го семетра', reply_markup=keyboard)
         
         
-def register_fio(message):
+def register_fio(callback):
     connect = sqlite3.connect('feedback.sql')
     cur = connect.cursor()
-    cur.execute('UPDATE data SET fio = "%s" WHERE id = %d ' % (message.text, message.chat.id))
+    cur.execute('UPDATE student SET fio = "%s" WHERE chatid = %d ' % (callback.text, callback.chat.id))
     connect.commit()
     cur.close()
     connect.close()
-    btn = types.InlineKeyboardButton('Пройти опрос', callback_data='complete_the_survey')
-    keyboard= types.InlineKeyboardMarkup()
-    keyboard.row(btn)
-    bot.send_message(message.chat.id, 'Отлично! Теперь ты можешь пройти опрос о качестве обучения', reply_markup=keyboard)
+    specialization_choice(callback)
+    
 
+
+def registration(callback):
+    bot.edit_message_text('Отправь мне своё ФИО', callback.message.chat.id, callback.message.message_id)
+    bot.register_next_step_handler(callback.message, register_fio)
+
+  
+        
+def specialization_choice(callback):
+    keyboard = specializations_buttons()
+    bot.send_message(callback.chat.id, "Выберите ваше направление:", reply_markup=keyboard)       
+    
 
 def semester_choice(callback):
     keyboard = types.InlineKeyboardMarkup(row_width=1)
     for key in DISCIPLINES.keys():
         callback_message = str(key) + '_semester'
         keyboard.add(types.InlineKeyboardButton(key, callback_data=callback_message))
-    bot.send_message(callback.message.chat.id, 'Ты выбрал группу 5140904/40401.\n\nО каком семестре ты хочешь оставить отзыв?', reply_markup=keyboard)
+    bot.send_message(callback.message.chat.id, 'О каком семестре ты хочешь оставить отзыв?', reply_markup=keyboard)
     
                
 def discplines_choice(callback):
@@ -126,16 +151,15 @@ def specializations_buttons():
     
 def group_code_buttons(group_code):
     keyboard = types.InlineKeyboardMarkup(row_width=1)
-    for group in GROUPS[group_code]:
-        callback_message = str(GROUPS[group_code].index(group)+1) + '_course'
-        keyboard.add(types.InlineKeyboardButton(group, callback_data = callback_message))
+    for key, value in GROUPS[group_code].items():
+        callback_message = key
+        keyboard.add(types.InlineKeyboardButton(value, callback_data = callback_message))
     return keyboard
 
 
 def discplines_button(semester):
     keyboard = types.InlineKeyboardMarkup(row_width=1)
     for discipline in DISCIPLINES[semester]:
-        #callback_message = discipline
         keyboard.add(types.InlineKeyboardButton(discipline, callback_data = discipline))
     return keyboard
 
